bed6e4d2c8cc7a0762adbad289469d27695cf7c7
Namra96 <Namra96@users.noreply.github.com>
2018-12-09 15:43:26 +0100

    camapp integrated with the android application

--- ConnectedDevProject/app/src/main/java/com/example/namragill/connecteddevproject/DashFragment.java
+++ ConnectedDevProject/app/src/main/java/com/example/namragill/connecteddevproject/DashFragment.java
@@ -1,13 +1,17 @@
 package com.example.namragill.connecteddevproject;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 /**
  * Created by namragill on 2018-11-14.
@@ -15,21 +19,44 @@ import android.widget.TextView;
 
 public class DashFragment extends Fragment {
 
-    TextView textView;
-    ImageView imageView;
+
+    private RecyclerView recyclerViewMain;
+    private ArrayList<Bitmap> images = new ArrayList<>();
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerViewAdapter adapter;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_dashboard,container,false);
+        View view = inflater.inflate(R.layout.recycler_view,container,false);
         initiatecomponents(view);
         return view;
     }
 
     private void initiatecomponents(View view) {
-        imageView =(ImageView) view.findViewById(R.id.imageView);
-        textView = (TextView)view.findViewById(R.id.maindashboard);
-        textView = (TextView)view.findViewById(R.id.maindashboard);
-        textView.setText(R.string.image_no);
+        recyclerViewMain = (RecyclerView) view.findViewById(R.id.rec_view);
+        try {
+            AsyncTaskGetImage getImages = new AsyncTaskGetImage(this);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setGridLayoutManager() {
+        recyclerViewMain.setHasFixedSize(true);
+        mLayoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerViewMain.setLayoutManager(mLayoutManager);
+        adapter = new GridLayoutAdapter(getActivity(), images);
+    }
+
+    private void setAdapter() {
+        recyclerViewMain.setAdapter(adapter);
+    }
+
+    public void setImage(ArrayList<Bitmap> image) {
+        this.images=image;
+        setGridLayoutManager();
+        setAdapter();
     }
 }
