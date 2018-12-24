f1cb82843c5657a731be9a46aaf8d54e42671f0a
Unknown <yurdaer.dalkic@hotmail.com>
2018-12-24 10:58:09 +0100

    My android project. Check how to implement a service

---
+++ Android ex-project/Asya/app/src/main/java/com/example/yurdaer/asya/Constants.java
@@ -0,0 +1,19 @@
+package com.example.yurdaer.asya;
+
+/**
+ * Created by YURDAER on 2018-01-20.
+ */
+
+public class Constants {
+    /* ----------- Constants for MQTT ----------- */
+    public static final String MQTT_BROKER_URL = "tcp://m23.cloudmqtt.com:10941";
+
+    public static final String PUBLISH_TOPIC = "Baby/Client";
+
+    public static final String IN_TOPIC = "Baby/Master";
+
+    public static  final  String PASSWORD ="JVPMD0qw7ij4";
+
+    public static final String CLIENT_ID = "dqaqegod";
+
+}
